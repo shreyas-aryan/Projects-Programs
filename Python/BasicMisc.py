b1e64d0d421bb6b0bff62d84bfdd8f9eb775f7fa
shreyas-aryan <ishreyas4567@gmail.com>
2025-12-30 20:25:45 +0530

    Create BasicMisc.py

---
+++ Python/BasicMisc.py
@@ -0,0 +1,199 @@
+#Variables--------------------------------------------------------------------------
+first="shreyas"
+last="aryan"
+grade='A'
+percent=87.2
+rank=2
+passed=True
+if passed:
+    print(f"{first} {last} has passed with grade {grade}, {percent}% and {rank}nd rank")
+else:
+    print(f"u failed, so no details to be provided for {first} {last}")
+
+#Oddeven---------------------------------------------------------------------------
+a=int(input("Enter a value to check if it's Odd or Even: "))
+if a%2==0:
+    print(f"Number {a} is Even")
+else:
+    print(f"Number {a} is Odd")
+
+#+ve,0,-ve-------------------------------------------------------------------------
+a=int(input("Enter a value: "))
+if a>=0:
+    if a==0:
+        print(f"{a} is a Zero Value")
+    else:
+        print(f"{a} is a +ve value")
+else:
+    print(f"{a} is a -ve value")
+
+#Rectangle peri, area----------------------------------------------------------------
+a=float(input("Enter Length: "))
+b=float(input("Enter Breadth: "))
+area=a*b
+peri=2*(a+b)
+print(f"The Perimeter of length {a} and breadth {b} rectangle is {peri}")
+print(f"The Area of length {a} and breadth {b} rectangle is {area}")
+
+#factorial---------------------------------------------------------------------------
+a=int(input("Enter value for factorial: "))
+fact=1
+for i in range(2,a+1):
+    fact=fact*i
+print(f"{fact}")
+
+#n number's table---------------------------------------------------------------------
+a=int(input("Enter the number for it's table: "))
+for i in range(1,11):
+    print(f"{a} * {i} = {a*i}")
+
+#simple interest----------------------------------------------------------------------
+a=float(input("Enter Principle amt: "))
+b=float(input("Enter Rate of interest(in percent): "))
+c=float(input("Enter Time(in yrs): "))
+si=(a*b*c)/100
+print(f"Principle: {a}")
+print(f"Rate: {b:.2f}")
+print(f"Time: {c:.2f}")
+print(f"Simple interest: {si:.2f}")
+amt=a+si
+print(f"TOTAL AMOUNT: {amt:.2f}")
+
+#compount interest--------------------------------------------------------------------
+a=float(input("Enter Principle amt: "))
+b=float(input("Enter Rate of interest(in percent): "))
+c=float(input("Enter Time(in yrs): "))
+print(f"Principle: {a}")
+print(f"Rate: {b:.2f}")
+print(f"Time: {c:.2f}")
+amt=a*((1+(b/100))**c)
+ci=amt-a
+print(f"Compound interest: {ci:.2f}")
+print(f"TOTAL AMOUNT: {amt:.2f}")
+
+#circumference and area of circle-----------------------------------------------------
+import math
+r=float(input("Enter radius: "))
+cir=2*math.pi*r
+area=math.pi*(r**2)
+print(f"Circumference {cir:.2f} and Area {area:.2f} of circle with radius {r:.2f}")
+
+#hypotenouse of a triangle-----------------------------------------------------------
+#sqrt(a**2+b**2)
+import math
+a=float(input("Enter side A: "))
+b=float(input("Enter side B: "))
+h=math.sqrt((a**2)+(b**2))
+print(f"Side C: {h} Side A: {a} Side B: {b}")
+
+#Armstrong---------------------------------------------------------------------------
+a=int(input("enter a number: "))
+temp=a
+sum=0
+n=len(str(a))
+while temp>0:
+    d=temp%10
+    sum=sum+(d**n)
+    temp=temp//10
+if sum==a:
+    print(a,"is an armstrong number")
+else:
+    print(a,"is not an armstrong number")
+
+#OPERATORS-------------------------------------------------------------------------
+print("Shreyas Aryan-00521402024")
+a=int(input("Enter Value: "))
+b=int(input("Enter 2nd Value: "))
+#arithmetic operations
+print(f"\nARITHMETIC OPERATIONS")
+print(f"{a} + {b} (sum) = {a+b}")
+print(f"{a} - {b} (difference) = {a-b}")
+print(f"{a} * {b} (multiplication) = {a*b}")
+print(f"{a} / {b} (division) = {a/b}")
+print(f"{a} ** {b} (exponent) = {a**b}")
+print(f"{a} // {b} (floor division) = {a//b}")
+print(f"{a} % {b} (modules) = {a%b}")
+#comparison operations
+print(f"\nCOMPARISON OPERATIONS")
+print(f"{a} >= {b} = {a>=b}")
+print(f"{a} != {b} = {a!=b}")
+print(f"{a} < {b} = {a<b}")
+#bitwise operations
+print(f"\nBITWISE OPERATIONS")
+print(f"{a} << {b} (left shift) = {a<<b}")
+print(f"{a} >> {b} (right shift) = {a>>b}")
+print(f"~{a} (not) = {~a}")
+print(f"{a} & {b} (and) = {a&b}")
+print(f"{a} | {b} (or) = {a|b}")
+print(f"{a} ^ {b} (xor) = {a^b}")
+#assignment operations
+print(f"\nASSIGNMENT OPERATIONS")
+a**=b
+print(f"Value of A after a**=b: {a}")
+a%=b
+print(f"Value of A after a%=b: {a}")
+a//=b
+print(f"Value of A after a//=b: {a}")
+
+#weight converting----------------------------------------------------------
+print("Weight Convertor")
+print("1.KGS to LBS")
+print("2.LBS to KGS")
+n=int(input("Enter your choice (1/2): "))
+if n==1:
+    kg=int(input("Enter weight in KGS: "))
+    lb=kg*2.25
+    print(f"{kg}kgs into lbs is {round(lb,3)}lbs")
+elif n==2:
+    lb=int(input("Enter weight in LBS: "))
+    kg=lb/2.25
+    print(f"{lb}lbs into kgs is {round(kg,3)}kgs")
+else:
+    print("INVALID OPTION!")
+
+#temp converting--------------------------------------------------------------
+print("Temp Convertor")
+print("1.C to F")
+print("2.F to C")
+n=int(input("Enter your choice (1/2): "))
+if n==1:
+    c=int(input("Enter in Celcius: "))
+    f=(c*(9/5))+32
+    print(f"{round(c,2)}C converts into {round(f,2)}F")
+elif n==2:
+    f=int(input("Enter in Farenheit: "))
+    c=(f-32)*(5/9)
+    print(f"{round(f,2)}F converts into {round(c,2)}C")
+else:
+    print("INVALID OPTION!")
+
+#ternary operator-------------------------------------------------------------
+a=int(input("Enter value: "))
+b=int(input("Enter 2nd value: "))
+print("A is greater" if a>b else "B is greater")
+
+#break at 10 sum of it--------------------------------------------------------
+n=int(input("Enter value for nothing: "))
+sum=0
+for i in range(1,n+1,1):
+    sum=sum+i
+    if i==10:
+        break
+print(sum)
+
+#MINIMUM NUMBER---------------------------------------------------------------
+a=13
+b=23
+c=2
+print(f"{a}:A\t{b}:B\t{c}:C")
+if a<b:
+    if a<c:
+        min=a
+    else:
+       min=c
+else:
+    if b<c:
+        min=b
+    else:
+        min=c
+print(f"{min} is the Minimum Number")
